3fdd317d73a407dbd6ad9e08fb63af1d9179d5b0
xc2 <xc2@users.noreply.github.com>
2024-04-19 01:12:32 +0800

    feat: unocss

--- rspack/unocss-webpack/rspack.config.js
+++ rspack/unocss-webpack/rspack.config.js
@@ -1,41 +1,56 @@
-const UnoCSS = require("@unocss/webpack").default;
 const rspack = require("@rspack/core");
 const NodePath = require("node:path");
 
-module.exports = /** @type {import('@rspack/core').Configuration} */ ({
-  mode: "development",
-  entry: "./src/index.jsx",
-  devtool: false,
-  module: {
-    rules: [
-      { test: /\.css$/, type: "css" },
+module.exports = async function config() {
+  const { default: UnoCSS } = await import("@unocss/webpack");
+  return /** @type {import('@rspack/core').Configuration} */ ({
+    mode: "development",
+    entry: "./src",
+    devtool: false,
+    module: {
+      rules: [{ test: /\.css$/, type: "css" }],
+    },
+    resolve: {
+      alias: {
+        // workaround for rspack
+        // "uno.css$": NodePath.resolve(__dirname, "__uno.css"),
+        // "__uno.css$": "data:",
+      },
+    },
+    optimization: { realContentHash: true },
+    plugins: [
+      UnoCSS(),
+      new rspack.HtmlRspackPlugin({
+        template: NodePath.resolve(__dirname, "public", "index.html"),
+      }),
       {
-        test: /\.jsx?$/,
-        use: [
-          {
-            loader: "builtin:swc-loader",
-            options: {
-              jsc: {
-                parser: { jsx: true, syntax: "ecmascript" },
-                transform: { react: { runtime: "automatic" } },
-              },
-            },
-          },
-        ],
+        apply(compiler) {
+          const VIRTUAL_ENTRY_ALIAS = [
+            /^(?:virtual:)?uno(?::(.+))?\.css(\?.*)?$/,
+          ];
+          const RESOLVED_ID_WITH_QUERY_RE =
+            /[\/\\]__uno(?:(_.*?))?\.css(\?.*)?$/;
+          function resolveId(id) {
+            if (id.match(RESOLVED_ID_WITH_QUERY_RE)) return id;
+            for (const alias of VIRTUAL_ENTRY_ALIAS) {
+              const match = id.match(alias);
+              if (match) {
+                return match[1] ? `/__uno_${match[1]}.css` : "/__uno.css";
+              }
+            }
+          }
+          compiler.hooks.normalModuleFactory.tap("a", (nmf) => {
+            const vfs = compiler.options.plugins.slice(-1)[0];
+            nmf.hooks.beforeResolve.tap("a", (nmf) => {
+              const resolved = resolveId(nmf.request);
+              if (resolved) {
+                vfs.writeModule(resolved, "");
+                nmf.request = resolved;
+              }
+            });
+          });
+        },
       },
     ],
-  },
-  resolve: {
-    alias: {
-      // workaround for rspack
-      "uno.css$": NodePath.resolve(__dirname, "__uno.css"),
-    },
-  },
-  optimization: { realContentHash: true },
-  plugins: [
-    new rspack.HtmlRspackPlugin({
-      template: NodePath.resolve(__dirname, "public", "index.html"),
-    }),
-    UnoCSS(),
-  ],
-});
+  });
+};
--- rspack/unocss-webpack/uno.config.ts
+++ rspack/unocss-webpack/uno.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from "unocss";
 import { presetUno } from "@unocss/preset-uno";
 
 export default defineConfig({
+  include: ["src/**/*.{js,jsx,ts,tsx,html}", "public/*.html"],
   presets: [presetUno()],
 });
